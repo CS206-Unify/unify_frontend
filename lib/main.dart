a9a108a5486dfa447066c6b16b21c4aab8ca67fa
Bk49 <ericngyongwei@gmail.com>
2024-02-14 00:47:56 +0800

    Use the router in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:unify/utils/theme/theme.dart';
+import 'package:unify/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Unify',
       darkTheme: ThemeData.from(
           colorScheme: const MaterialTheme().dark().colorScheme,
@@ -20,86 +21,7 @@ class MyApp extends StatelessWidget {
           colorScheme: const MaterialTheme().light().colorScheme,
           textTheme: const MaterialTheme().text()),
       themeMode: ThemeMode.system,
-      home: const HomePage(title: 'Unify'),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have clicked the button this many times:',
-            ),
-            Text(
-              'displayLarge',
-              style: Theme.of(context).textTheme.displayLarge,
-            ),
-            Text(
-              'displayMedium',
-              style: Theme.of(context).textTheme.displayMedium,
-            ),
-            Text(
-              'displaySmall',
-              style: Theme.of(context).textTheme.displaySmall,
-            ),
-            Text(
-              'headlineLarge',
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-            Text(
-              'headlineMedium',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text(
-              'headlineSmall',
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-            Text(
-              'titleLarge',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            Text(
-              'titleMedium',
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            Text(
-              'titleSmall',
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      routerConfig: router,      
     );
   }
 }
