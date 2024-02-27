15753bb6316606861f91d8c32f59beb057b976cc
Bk49 <ericngyongwei@gmail.com>
2024-02-27 21:40:14 +0800

    Modify register function to display appropriate snackbar and store the token upon registration

--- lib/data/unify-spring/authentication.dart
+++ lib/data/unify-spring/authentication.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+// import 'package:go_router/go_router.dart';
 import 'package:http/http.dart' as http;
 import 'package:unify/data/unify-spring/serializers/authentication/registration_serializer.dart';
+import 'package:unify/data/unify-spring/serializers/authentication/token_serializer.dart';
 import 'package:unify/data/unify-spring/serializers/error/common_error_serializer.dart';
 import 'package:unify/main.dart';
+import 'package:unify/router.dart';
 import 'package:unify/utils/constants/unify_backend.dart' as unify_client;
+import 'package:unify/utils/local_storage/secure_storage.dart';
 
 void register(RegisterRequest registerRequest, BuildContext context) async {
   try {
@@ -17,8 +20,20 @@ void register(RegisterRequest registerRequest, BuildContext context) async {
         },
         body: jsonEncode(registerRequest.toJson()));
 
-    if (res.statusCode == 201 && context.mounted) {
-      context.go("/home");
+    if (res.statusCode == 201) {
+      final String? token =
+          TokenResponse.fromJson(json.decode(res.body) as Map<String, dynamic>)
+              .token;
+      
+      if (token == null || token.isEmpty) {
+        SnackBarService.showSnackBar(
+            content:
+                "There is an error trying to set the user token, please try registering again");
+        return;
+      }
+
+      await SecureStorage.setToken(token);
+      router.go("/home");
     } else {
       SnackBarService.showSnackBar(
           content: CommonError.fromJson(
