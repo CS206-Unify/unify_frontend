d71cd45330dcaeea82ffff401b246a2a04548a4e
adversesg <faviancheong22@gmail.com>
2024-02-28 17:37:19 +0800

    Updated Top App Bar with BS logo

--- lib/widgets/profile/screens/brawlstarsprofile_page.dart
+++ lib/widgets/profile/screens/brawlstarsprofile_page.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:unify/widgets/common/nav/bottom_navigation_bar.dart';
 import 'package:unify/widgets/common/nav/top_app_bar.dart';
 
 class BrawlStarsProfilePage extends StatefulWidget {
-  const BrawlStarsProfilePage({super.key});
+  const BrawlStarsProfilePage({super.key, this.title, this.game, this.profileImg});
+  final String? title;
+  final String? game;
+  final String? profileImg;
 
   @override
   State<BrawlStarsProfilePage> createState() => _BrawlStarsProfilePageState();
+
+  @override
+  Size get preferredSize => Size.fromHeight(AppBar().preferredSize.height);
 }
 
 class _BrawlStarsProfilePageState extends State<BrawlStarsProfilePage> {
@@ -14,7 +21,45 @@ class _BrawlStarsProfilePageState extends State<BrawlStarsProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
+        backgroundColor: Theme.of(context).colorScheme.surface,
+      title: Text(
+        widget.title ?? "Brawl Stars Profile",
+        style: TextStyle(
+          color: Theme.of(context).colorScheme.primary,
+        ),
+      ),
+      leading: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+          decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.onPrimary,
+              borderRadius: BorderRadius.circular(100.0),
+              border: Border.all(
+                  color: Theme.of(context).colorScheme.onPrimary, width: 1.0)),
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(100.0),
+              child: Image(
+                  image: AssetImage(widget.game == "bs"
+                      ? "assets/images/logos/BS.png"
+                      : widget.game == "mlbb"
+                          ? "assets/images/logos/MLBB.jpg"
+                          : widget.game == "codm"
+                              ? "assets/images/logos/CODM.webp"
+                              : "assets/images/logos/BS.png")))),
+      actions: [
+        InkWell(
+            onTap: () => context.go('/profile'),
+            child: Container(
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: widget.profileImg != null
+                    ? Image.network(widget.profileImg!)
+                    : const Image(
+                        image: AssetImage("assets/images/logos/CODM.webp")),
+              ),
+            ))
+      ],
       ),
       body: const Center(
           child: Column(
