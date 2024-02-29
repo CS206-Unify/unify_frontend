af89ca14be6c710ea87d2fc6346b397660bc7ff8
Bk49 <ericngyongwei@gmail.com>
2024-02-29 21:30:44 +0800

    Complete serializer for Profile Listing

--- lib/data/unify-spring/serializers/discover/profile_listing_serializer.dart
+++ lib/data/unify-spring/serializers/discover/profile_listing_serializer.dart
@@ -1,33 +1,30 @@
-// class ProfileListing {
-//   final String id;
-//   final String name;
-//   final int trophies;
-//   final int wins3v3;
-//   final String region;
-//   final String brawler;
-//   final int rank;
+class ProfileListing {
+  final String id;
+  final String name;
+  final int trophies;
+  final int wins3v3;
+  final String region;
+  final String brawler;
+  final int rank;
 
-//   ProfileListing( 
-//       {required this.id,
-//       required this.name,
-//       required this.trophies,
-//       required this.wins3v3,
-//       required this.region,
-//       required this.brawler, required this.rank});
+  ProfileListing(
+      {required this.id,
+      required this.name,
+      required this.trophies,
+      required this.wins3v3,
+      required this.region,
+      required this.brawler,
+      required this.rank});
 
-//   factory ProfileListing.fromJson(Map<String, dynamic> json){
-//     return switch(json){
-//       {
-//         '_id': String _id,
-//         'username': String username,
-//         'trophies': String
-//       } => ProfileListing(
-//         id: _id, 
-//         name: username, 
-//         trophies: trophies, 
-//         wins3v3: wins3v3, 
-//         region: region, 
-//         brawler: brawler), => throw const FormatException("Failed to load Profile Listing")
-//     };
-//   }
-// }
+  factory ProfileListing.fromMap(Map<String, dynamic> map) {
+    return ProfileListing(
+      id: map["_id"],
+      name: map["username"],
+      trophies: map["trophies"],
+      wins3v3: map["threeVThreeWins"],
+      region: map["region"],
+      brawler: map["bestBrawlerName"],
+      rank: map["bestBrawlerRank"],
+    );
+  }
+}
