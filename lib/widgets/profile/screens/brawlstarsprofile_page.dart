6b7c3dd07ff9f146124a27392380f65caea4fd9f
adversesg <faviancheong22@gmail.com>
2024-02-29 15:54:48 +0800

    Updated Region to use Dropdownmenu instead of dropdownbutton

--- lib/widgets/profile/screens/brawlstarsprofile_page.dart
+++ lib/widgets/profile/screens/brawlstarsprofile_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:unify/widgets/common/nav/bottom_navigation_bar.dart';
 import 'package:unify/widgets/common/nav/top_app_bar.dart';
 
 const List<String> list = <String>['Any','North America', 'Latin America', 'Brazil', 'Europe', 'Korea', 'Asia Pacific'];
 String dropdownValue = 'Any';
+var _controller = TextEditingController();
 
 class BrawlStarsProfilePage extends StatefulWidget {
   const BrawlStarsProfilePage({super.key, this.title, this.game, this.profileImg});
@@ -31,62 +31,88 @@ class _BrawlStarsProfilePageState extends State<BrawlStarsProfilePage> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            SizedBox(
+              height:20 ,
+            ),
             //Outside padding for Card
-            Padding(
-              padding: const EdgeInsets.all(15.0),
+            // Padding(
+            //   padding: const EdgeInsets.all(15.0),
 
-              //Card for Region
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 10, 0, 0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        width: 300,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Region',
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                color: Theme.of(context).colorScheme.inverseSurface,
-                              ),
-                            ), 
-                            DropdownButton<String>(
-                              isExpanded: true,
-                              items: list.map((String value) {
-                                return DropdownMenuItem<String>(
-                                  value: value,
-                                  child: Text(
-                                    value,
-                                    style: const TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.normal,
-                                    ),
-                                  ),
-                                );
-                              }).toList(),
-                              value: dropdownValue,
-                              onChanged: (String? newValue) {
-                                setState(() {
-                                  dropdownValue = newValue!;
-                                });
-                              },
-                              borderRadius: BorderRadius.zero,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+            //   //Card for Region
+            //   child: Card(
+            //     shape: RoundedRectangleBorder(
+            //       borderRadius: BorderRadius.circular(8.0),
+            //     ),
+            //     child: Padding(
+            //       padding: const EdgeInsets.fromLTRB(20, 10, 0, 0),
+            //       child: Row(
+            //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //         children: [
+            //           Container(
+            //             width: 300,
+            //             child: Column(
+            //               crossAxisAlignment: CrossAxisAlignment.start,
+            //               children: [
+            //                 Text(
+            //                   'Region',
+            //                   style: TextStyle(
+            //                     fontSize: 14.0,
+            //                     color: Theme.of(context).colorScheme.inverseSurface,
+            //                   ),
+            //                 ), 
+            //                 DropdownButton<String>(
+            //                   isExpanded: true,
+            //                   items: list.map((String value) {
+            //                     return DropdownMenuItem<String>(
+            //                       value: value,
+            //                       child: Text(
+            //                         value,
+            //                         style: const TextStyle(
+            //                           fontSize: 18,
+            //                           fontWeight: FontWeight.normal,
+            //                         ),
+            //                       ),
+            //                     );
+            //                   }).toList(),
+            //                   value: dropdownValue,
+            //                   onChanged: (String? newValue) {
+            //                     setState(() {
+            //                       dropdownValue = newValue!;
+            //                     });
+            //                   },
+            //                   borderRadius: BorderRadius.zero,
+            //                 ),
+            //               ],
+            //             ),
+            //           ),
+            //         ],
+            //       ),
+            //     ),
+            //   ),
+            // ),
+
+            //Attempt at Region Drop Down Menu instead
+            Center(
+              child: DropdownMenu(
+                label: const Text('Region'),
+                width: 350,
+                initialSelection: dropdownValue,
+                dropdownMenuEntries: list.map((String item) {
+                  return DropdownMenuEntry(
+                    value: item,
+                    label: item,
+                  );
+                }).toList(),
+                inputDecorationTheme: const InputDecorationTheme(
+                  filled: true, 
+                  constraints: BoxConstraints.expand()
                 ),
               ),
             ),
+            
+            SizedBox(height: 20,),
+
+            //Title for personal Bio
             Padding(
               padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
               child: Text('Personal Bio',
@@ -96,6 +122,29 @@ class _BrawlStarsProfilePageState extends State<BrawlStarsProfilePage> {
                 ),
               ),
             ),
+            
+              //Card for Personal Bio
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                child: Stack(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        labelText: 'Brawl Stars Bio',
+                        hintText: 'Enter Bio',
+                        alignLabelWithHint: true,
+                        floatingLabelBehavior: FloatingLabelBehavior.always,
+                        suffixIcon: IconButton(
+                          onPressed: _controller.clear,
+                          icon: const Icon(Icons.highlight_off),
+                        ),
+                      ),
+                      maxLines: 8,
+                    ),
+                  ],
+                ),
+              ),
           ],
         ),
       ),
