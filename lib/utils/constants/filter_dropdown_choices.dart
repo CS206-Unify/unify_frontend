9534bc9a9ebd890fe92f004d62533c971e3873c5
Bk49 <ericngyongwei@gmail.com>
2024-02-29 16:11:14 +0800

    Add a constants file for all the filter dropdown choices

---
+++ lib/utils/constants/filter_dropdown_choices.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+const List<DropdownMenuEntry> regionEntries = [
+  DropdownMenuEntry(value: "", label: "Any"),
+  DropdownMenuEntry(value: "NA", label: "North America"),
+  DropdownMenuEntry(value: "LS", label: "Latam South"),
+  DropdownMenuEntry(value: "EU", label: "Europe/Middle East/Africa"),
+  DropdownMenuEntry(value: "AP", label: "Asia Pacific"),
+  DropdownMenuEntry(value: "CN", label: "Mainland China"),
+];
+
+const List<DropdownMenuEntry> trophiesEntries = [
+  DropdownMenuEntry(value: 0, label: "Any"),
+  DropdownMenuEntry(value: 10000, label: "10000+"),
+  DropdownMenuEntry(value: 20000, label: "20000+"),
+  DropdownMenuEntry(value: 30000, label: "30000+"),
+  DropdownMenuEntry(value: 40000, label: "40000+"),
+  DropdownMenuEntry(value: 50000, label: "50000+"),
+  DropdownMenuEntry(value: 60000, label: "60000+"),
+  DropdownMenuEntry(value: 70000, label: "70000+"),
+  DropdownMenuEntry(value: 75000, label: "75000+"),
+  DropdownMenuEntry(value: 80000, label: "80000+"),
+  DropdownMenuEntry(value: 85000, label: "85000+"),
+  DropdownMenuEntry(value: 90000, label: "90000+"),
+  DropdownMenuEntry(value: 95000, label: "95000+"),
+  DropdownMenuEntry(value: 100000, label: "100000+"),
+];
+
+const List<DropdownMenuEntry> wins3v3Entries = [
+  DropdownMenuEntry(value: 0, label: "Any"),
+  DropdownMenuEntry(value: 5000, label: "5000+"),
+  DropdownMenuEntry(value: 10000, label: "10000+"),
+  DropdownMenuEntry(value: 15000, label: "15000+"),
+  DropdownMenuEntry(value: 20000, label: "20000+"),
+  DropdownMenuEntry(value: 25000, label: "25000+"),
+  DropdownMenuEntry(value: 30000, label: "30000+"),
+  DropdownMenuEntry(value: 35000, label: "35000+"),
+  DropdownMenuEntry(value: 40000, label: "40000+"),
+  DropdownMenuEntry(value: 45000, label: "45000+"),
+];
+
+const List<DropdownMenuEntry> wins2v2Entries = [
+  DropdownMenuEntry(value: 0, label: "Any"),
+  DropdownMenuEntry(value: 500, label: "500+"),
+  DropdownMenuEntry(value: 1000, label: "1000+"),
+  DropdownMenuEntry(value: 1500, label: "1500+"),
+  DropdownMenuEntry(value: 2000, label: "2000+"),
+  DropdownMenuEntry(value: 2500, label: "2500+"),
+  DropdownMenuEntry(value: 3000, label: "3000+"),
+  DropdownMenuEntry(value: 3500, label: "3500+"),
+  DropdownMenuEntry(value: 4000, label: "4000+"),
+  DropdownMenuEntry(value: 4500, label: "4500+"),
+  DropdownMenuEntry(value: 5000, label: "5000+"),
+];
+
+const List<DropdownMenuEntry> soloWinsEntries = [
+  DropdownMenuEntry(value: 0, label: "Any"),
+  DropdownMenuEntry(value: 500, label: "500+"),
+  DropdownMenuEntry(value: 1000, label: "1000+"),
+  DropdownMenuEntry(value: 1500, label: "1500+"),
+  DropdownMenuEntry(value: 2000, label: "2000+"),
+  DropdownMenuEntry(value: 2500, label: "2500+"),
+  DropdownMenuEntry(value: 3000, label: "3000+"),
+  DropdownMenuEntry(value: 3500, label: "3500+"),
+];
