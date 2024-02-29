e8649cc33e40a540704b403e077228790a3abc60
Bk49 <ericngyongwei@gmail.com>
2024-02-29 21:32:11 +0800

    Integrate changes from formatter to correctly display the top brawler of the user

--- lib/widgets/discover/card/player_card_sub_widgets/player_top_brawler.dart
+++ lib/widgets/discover/card/player_card_sub_widgets/player_top_brawler.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:unify/utils/formatter/brawler_formatter.dart';
 
 class PlayerTopBrawler extends StatelessWidget {
   const PlayerTopBrawler(
@@ -18,7 +19,7 @@ class PlayerTopBrawler extends StatelessWidget {
             borderRadius: BorderRadius.circular(100),
             child: Image(
                 image: AssetImage(
-                    "assets/images/brawlstars/brawler_icons/$brawler.webp")),
+                    "assets/images/brawlstars/brawler_icons/${brawlerBackendToAssetImageStr(brawler)}")),
           ),
         ),
         Image(
