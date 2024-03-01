66839f61778e3aef39c4100bee0b7977ac089499
Bk49 <ericngyongwei@gmail.com>
2024-03-01 21:35:30 +0800

    Create simple battle history serializer for storing number of wins, loses and draws of the user

---
+++ lib/data/unify-spring/serializers/discover/battle_history_serializer.dart
@@ -0,0 +1,11 @@
+class BattleHistory {
+  final int wins;
+  final int loses;
+  final int draws;
+
+  BattleHistory({
+    required this.wins,
+    required this.loses,
+    required this.draws,
+  });
+}
