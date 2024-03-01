a9ac56ca988b26fdd0825c8115cb5c8419d192d5
Bk49 <ericngyongwei@gmail.com>
2024-03-01 21:38:51 +0800

    Add future builder to player details page to integrate with api integration functions

--- lib/widgets/discover/screens/playerdetails_page.dart
+++ lib/widgets/discover/screens/playerdetails_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:unify/data/unify-spring/serializers/discover/brawler_details_serializer.dart';
+import 'package:unify/data/unify-spring/discover.dart';
 import 'package:unify/widgets/common/nav/bottom_navigation_bar.dart';
 import 'package:unify/widgets/common/nav/top_app_bar.dart';
 import 'package:unify/widgets/discover/card/player_details_hero_banner.dart';
@@ -9,7 +9,12 @@ import 'package:unify/widgets/discover/section/player_details_brawlers_section.d
 import 'package:unify/widgets/discover/section/player_details_statistics_section.dart';
 
 class PlayerDetailsPage extends StatefulWidget {
-  const PlayerDetailsPage({super.key});
+  const PlayerDetailsPage({
+    super.key,
+    required this.userId,
+  });
+
+  final String userId;
 
   @override
   State<PlayerDetailsPage> createState() => _PlayerDetailsPageState();
@@ -20,46 +25,63 @@ class _PlayerDetailsPageState extends State<PlayerDetailsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const TopAppBar(game: "bs", title: "Player Details"),
-      body: Column(children: [
-        const PlayerDetailsHeroBanner(
-            region: "AP",
-            username: "Benjamin Gan",
-            trophies: 114918,
-            joinDate: "January 20, 2024"),
-        Expanded(
-          child: Container(
-            margin: const EdgeInsetsDirectional.only(start: 16, end: 16),
-            child: SingleChildScrollView(
-              child: Wrap(
-                direction: Axis.vertical,
-                spacing: 24,
-                children: [
-                  PlayerDetailsStatisticsSection(
-                      trophies: 114918,
-                      soloWins: 776,
-                      wins2v2: 1051,
-                      wins3v3: 50212),
-                  PlayerDetailsBioSection(
-                      bio:
-                          "I love Brawl Stars. I really really really really really really really really really really really really really really really really really really like it "),
-                  PlayerDetailsBattleHistorySection(
-                      wins: 86, loses: 13, draws: 1),
-                  PlayerDetailsBrawlersSection(brawlers: [
-                    BrawlerDetails(name: "SHELLY", rank: 22, trophies: 500),
-                    BrawlerDetails(name: "SHELLY", rank: 22, trophies: 500),
-                    BrawlerDetails(name: "SHELLY", rank: 22, trophies: 500),
-                    BrawlerDetails(name: "SHELLY", rank: 22, trophies: 500),
-                    BrawlerDetails(name: "SHELLY", rank: 22, trophies: 500),
-                    BrawlerDetails(name: "SHELLY", rank: 22, trophies: 500),
-                    BrawlerDetails(name: "SHELLY", rank: 22, trophies: 500),
-                    BrawlerDetails(name: "SHELLY", rank: 22, trophies: 500)
-                  ])
-                ],
-              ),
-            ),
-          ),
-        )
-      ]),
+      body: FutureBuilder(
+          future: getProfileById(widget.userId),
+          builder: (context, profileSnapshot) {
+            if (profileSnapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (profileSnapshot.hasError) {
+              return Text("Error: ${profileSnapshot.error}");
+            } else {
+              return Column(children: [
+                PlayerDetailsHeroBanner(
+                    region: profileSnapshot.data!.region,
+                    username: profileSnapshot.data!.username,
+                    trophies: profileSnapshot.data!.trophies,
+                    joinDate: profileSnapshot.data!.joinDate),
+                Expanded(
+                  child: Container(
+                    margin:
+                        const EdgeInsetsDirectional.only(start: 16, end: 16),
+                    child: SingleChildScrollView(
+                      child: Wrap(
+                        direction: Axis.vertical,
+                        spacing: 24,
+                        children: [
+                          PlayerDetailsStatisticsSection(
+                              trophies: profileSnapshot.data!.trophies,
+                              soloWins: profileSnapshot.data!.winsSolo,
+                              wins2v2: profileSnapshot.data!.wins2v2,
+                              wins3v3: profileSnapshot.data!.wins3v3),
+                          PlayerDetailsBioSection(
+                              bio: profileSnapshot.data!.bio),
+                          FutureBuilder(
+                            future: getBrawlStarHistory(
+                                profileSnapshot.data!.playerTag),
+                            builder: (context, battleHistorySnapshot) {
+                              if (battleHistorySnapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                return const Center(
+                                    child: CircularProgressIndicator());
+                              } else if (battleHistorySnapshot.hasError) {
+                                return Text(
+                                    "Error: ${battleHistorySnapshot.error}");
+                              } else {
+                                return PlayerDetailsBattleHistorySection(
+                                    history: battleHistorySnapshot.data!);
+                              }
+                            },
+                          ),
+                          PlayerDetailsBrawlersSection(
+                              brawlers: profileSnapshot.data!.brawlers)
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ]);
+            }
+          }),
       bottomNavigationBar: const BottomNavBar(
         current: "discover",
       ),
