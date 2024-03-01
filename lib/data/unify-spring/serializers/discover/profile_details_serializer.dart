032b360774e026882287b06b2171531794c9d1af
Bk49 <ericngyongwei@gmail.com>
2024-03-01 21:37:44 +0800

    Fix serializer for profile details to reference to the correct fields

--- lib/data/unify-spring/serializers/discover/profile_details_serializer.dart
+++ lib/data/unify-spring/serializers/discover/profile_details_serializer.dart
@@ -7,14 +7,13 @@ class ProfileDetails {
   final int trophies;
   final String imgUrl;
   final String bio;
-  // final int wins;
-  // final int loses;
-  // final int draws;
+  final String joinDate;
   final List<String> teams;
   final int wins3v3;
   final int wins2v2;
   final int winsSolo;
   final List<BrawlerDetails> brawlers;
+  final String playerTag;
 
   ProfileDetails(
       {required this.id,
@@ -23,31 +22,29 @@ class ProfileDetails {
       required this.trophies,
       required this.imgUrl,
       required this.bio,
-      // required this.wins,
-      // required this.loses,
-      // required this.draws,
+      required this.joinDate,
       required this.teams,
       required this.wins3v3,
       required this.wins2v2,
       required this.winsSolo,
-      required this.brawlers});
+      required this.brawlers, required this.playerTag});
 
   factory ProfileDetails.fromMap(Map<String, dynamic> map) {
     return ProfileDetails(
         id: map["_id"],
+        playerTag: map["bsProfile"]["playerTag"],
         username: map["username"],
         region: map["bsProfile"]["region"],
-        trophies: map["bsProfile"]["trophies"],
+        trophies: map["bsProfile"]["player"]["trophies"],
         imgUrl: map["imageString"],
         bio: map["bsProfile"]["personalBio"],
-        // wins: wins,
-        // loses: loses,
-        // draws: draws,
-        teams: map["teams"],
-        wins3v3: map["threeVsThreeVictories"],
-        wins2v2: map["duoVictories"],
-        winsSolo: map["soloVictories"],
-        brawlers: (map["bsProfile"]["brawlers"] as List<Map<String, dynamic>>)
+        joinDate: map["userCreationDate"],
+        teams: (map["teams"] as List).map((item) => item as String).toList(),
+        wins3v3: map["bsProfile"]["player"]["threeVsThreeVictories"],
+        wins2v2: map["bsProfile"]["player"]["duoVictories"],
+        winsSolo: map["bsProfile"]["player"]["soloVictories"],
+        brawlers: (map["bsProfile"]["player"]["brawlers"]
+                as List)
             .map((item) => BrawlerDetails.fromJson(item))
             .toList());
   }
