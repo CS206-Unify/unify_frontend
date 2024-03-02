a24ff4d9486817f5434908db7b580c648b6b03f8
Shinkeishi <ashleyytanjx@gmail.com>
2024-03-03 03:58:52 +0800

    Uploaded integration icons

--- lib/widgets/profile/screens/profile_page.dart
+++ lib/widgets/profile/screens/profile_page.dart
@@ -4,38 +4,82 @@ import 'package:unify/widgets/profile/edit_profile_icon.dart';
 import 'package:unify/widgets/profile/top_profile_bar.dart';
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  String name = "Benjamin Gan";
+  String email = "benjamin.gan@gmail.com";
+
+  List<String> countries = [
+    'Country',
+    'Singapore',
+    'Indonesia',
+    'Malaysia',
+    'Philipines'
+  ];
+  String selectedCountry = 'Country';
+
   @override
   Widget build(BuildContext context) {
-    String name = "Benjamin Gan";
-    String email = "benjamin.gan@gmail.com";
-
     return Scaffold(
       appBar: const TopProfileBar(),
       body: Center(
-          child: Column(
-        children: <Widget>[
-          EditProfileIcon(),
-          Text(
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge
-                  ?.apply(color: Theme.of(context).colorScheme.primary),
-              '$name'),
-          Text(
+        child: Column(
+          children: <Widget>[
+            EditProfileIcon(),
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: Text(
+                name,
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge
+                    ?.apply(color: Theme.of(context).colorScheme.primary),
+              ),
+            ),
+            Text(
+              email,
               style: Theme.of(context)
                   .textTheme
                   .titleSmall
                   ?.apply(color: Theme.of(context).colorScheme.secondary),
-              '$email'),
-        ],
-      )),
+            ),
+            Container(
+              width: 200,
+              child: DropdownButton<String>(
+                isExpanded: true,
+                value: selectedCountry,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedCountry = newValue!;
+                  });
+                },
+                items: countries.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Integrations",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge
+                    ?.apply(color: Theme.of(context).colorScheme.primary),
+              ),
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: const BottomNavBar(current: ""),
     );
   }
