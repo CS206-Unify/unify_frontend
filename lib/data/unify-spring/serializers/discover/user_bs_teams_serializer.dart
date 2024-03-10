8017bb2fe7fd1fc56baf5d155926336b9983aa1e
Bk49 <ericngyongwei@gmail.com>
2024-03-10 15:36:40 +0800

    Create UserBsTeamsListing serializer to convert json to dart code for listing teams the user is currently in

---
+++ lib/data/unify-spring/serializers/discover/user_bs_teams_serializer.dart
@@ -0,0 +1,13 @@
+class UserBsTeamsListing {
+  final String id;
+  final String name;
+  final String imgUrl;
+
+  UserBsTeamsListing(
+      {required this.id, required this.name, required this.imgUrl});
+
+  factory UserBsTeamsListing.fromMap(Map<String, dynamic> map) {
+    return UserBsTeamsListing(
+        id: map["_id"], name: map["teamName"], imgUrl: map["imageString"]);
+  }
+}
