c1c75fd572f63736777cdc6d4f7f4f55c8c4294b
Bk49 <ericngyongwei@gmail.com>
2024-03-10 15:36:06 +0800

    (Temp) Create Future functions for integrating back end for teams dialog

--- lib/data/unify-spring/discover.dart
+++ lib/data/unify-spring/discover.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:logger/logger.dart';
 import 'package:unify/data/unify-spring/serializers/discover/profile_details_serializer.dart';
 import 'package:unify/data/unify-spring/serializers/discover/profile_listing_serializer.dart';
 import 'package:http/http.dart' as http;
+import 'package:unify/data/unify-spring/serializers/discover/user_bs_teams_serializer.dart';
 import 'package:unify/data/unify-spring/serializers/error/common_error_serializer.dart';
 import 'package:unify/main.dart';
 import 'package:unify/utils/constants/unify_backend.dart';
@@ -57,6 +59,60 @@ Future<ProfileDetails> getProfileById(String userId) async {
               .message!);
       throw Error();
     }
+  } catch (e) {
+    SnackBarService.showSnackBar(content: e.toString());
+    rethrow;
+  }
+}
+
+Future<List<UserBsTeamsListing>> getUserBsTeamsListing() async {
+  try {
+    List<UserBsTeamsListing> teams = [
+      UserBsTeamsListing(id: "a", name: "Team Ninja", imgUrl: ""),
+      UserBsTeamsListing(id: "b", name: "Rogue Esports", imgUrl: ""),
+      UserBsTeamsListing(id: "c", name: "Hornets Gaming", imgUrl: ""),
+      UserBsTeamsListing(id: "d", name: "ROM Esports Club", imgUrl: ""),
+      UserBsTeamsListing(id: "e", name: "Spartans Squad", imgUrl: ""),
+      UserBsTeamsListing(id: "f", name: "Evil Geniuses", imgUrl: ""),
+    ];
+
+    return teams;
+    // final res = await http.get(Uri.parse("$unifyProfileServiceUrl/team"),
+    //     headers: {
+    //       HttpHeaders.authorizationHeader:
+    //           "Bearer ${await SecureStorage.getToken()}"
+    //     });
+
+    // if (res.statusCode == 200) {
+    //   List<UserBsTeamsListing> teams =
+    //       (json.decode(res.body)["bsTeams"] as List)
+    //           .map((item) => UserBsTeamsListing.fromMap(item))
+    //           .toList();
+
+    //   return teams;
+    // }
+
+    // SnackBarService.showSnackBar(
+    //     content:
+    //         CommonError.fromJson(json.decode(res.body) as Map<String, dynamic>)
+    //             .message!);
+    // throw Error();
+  } catch (e) {
+    SnackBarService.showSnackBar(content: e.toString());
+    rethrow;
+  }
+}
+
+Future<void> inviteToTeamById(String teamId, String toAddUserId) async {
+  try {
+    // await http.post(
+    //     Uri.parse("$unifyTeamServiceUrl/$teamId/member/$toAddUserId"),
+    //     headers: {
+    //       HttpHeaders.authorizationHeader:
+    //           "Bearer ${await SecureStorage.getToken()}"
+    //     });
+    Logger().d("$unifyTeamServiceUrl/$teamId/member/$toAddUserId");
+    return;
   } catch (e) {
     SnackBarService.showSnackBar(content: e.toString());
     rethrow;
