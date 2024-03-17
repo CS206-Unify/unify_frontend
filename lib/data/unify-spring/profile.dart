b1785364427fc006c7d9417c3e9e34b827602233
Shinkeishi <ashleyytanjx@gmail.com>
2024-03-17 22:19:37 +0800

    Added putUserProfile API

--- lib/data/unify-spring/profile.dart
+++ lib/data/unify-spring/profile.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:unify/data/unify-spring/serializers/profile/userprofile_serializer.dart';
 import 'package:unify/model/profile/userprofile_model.dart';
 import 'package:unify/utils/local_storage/secure_storage.dart';
 
@@ -15,5 +16,30 @@ Future<UserProfile> getUserProfile() async {
     return UserProfile.fromJson(json.decode(response.body));
   } else {
     throw Exception('Failed to load user profile');
+  }
+}
+
+Future<void> putCountryForUserProfile(String? country) async {
+  final url = Uri.parse('http://10.0.2.2:8080/api/profile');
+  var userProfile = UserProfileRequest(
+    imageString: "null",
+    country: '$country',
+    language: "English",
+  );
+
+  final response = await http.put(
+    url,
+    headers: {
+      'Authorization': 'Bearer ${await SecureStorage.getToken()}',
+      'Content-Type': 'application/json',
+    },
+    body: json.encode(userProfile.toJson()),
+  );
+
+  if (response.statusCode == 200) {
+    print('PUT Request success!');
+    return;
+  } else {
+    throw Exception('Failed to update user profile');
   }
 }
