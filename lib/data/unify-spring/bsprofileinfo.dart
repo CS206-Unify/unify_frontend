9770b9376b9e1af4638a72f44e07d6a0469160b9
adversesg <faviancheong22@gmail.com>
2024-03-18 17:33:31 +0800

    Cleaned further

--- lib/data/unify-spring/bsprofileinfo.dart
+++ lib/data/unify-spring/bsprofileinfo.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:logger/logger.dart';
 import 'package:unify/data/unify-spring/serializers/authentication/bsprofile_serializer.dart';
 import 'package:unify/main.dart';
 import 'package:unify/utils/constants/unify_backend.dart';
