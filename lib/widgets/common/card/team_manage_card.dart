c4cad074cdcd6164beb3bf15e06cf5a014ce495c
Poh En Xi <enxi.poh.2022@scis.smu.edu.sg>
2024-03-18 21:27:17 +0800

    created and integrated manage_teams_page

--- lib/widgets/common/card/team_manage_card.dart
+++ lib/widgets/common/card/team_manage_card.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/src/misc/extensions.dart';
 
 class TeamManageCard extends StatelessWidget {
-  const TeamManageCard({super.key});
+
+  final String teamId;
+  final String teamName;
+  final String gameName;
+  final String region;
+  final String language;
+  final String imageString;
+  final int maxTeamSize;
+  final int memberListSize;
+
+  const TeamManageCard({super.key, required this.teamId, required this.teamName, required this.gameName, required this.region, required this.language, required this.imageString, required this.maxTeamSize, required this.memberListSize});
 
   @override
   Widget build(BuildContext context) {
@@ -9,75 +20,78 @@ class TeamManageCard extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(8.0),
       ),
-      child: Container(
-        width: 360,
-        height: 104,
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              top: 1,
-              right: 1,
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(8.0),
-                    topRight: Radius.circular(8.0)),
-                child: Container(
-                  color: Theme.of(context).colorScheme.secondaryContainer,
-                  width: 36.0,
-                  height: 36.0,
-                ),
-              ), //purple bg
-            ),
-            Positioned(
-              top: 4,
-              right: 4,
-              child: CircleAvatar(
-                radius: 15.0,
-                foregroundImage: AssetImage('assets/images/logos/BS.png')), //game logo on top right hand corner
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(12.0, 15.0, 12.0, 15.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 38.0,
-                    foregroundImage: (AssetImage('assets/images/logos/Unify.png')),
-                  ), //team avatar
-                  SizedBox(
-                    width: 10.0,
+      child: GestureDetector(
+        onTap: () => {context.go("/edit_team/:$teamId")}, 
+        child: SizedBox(
+          width: 360,
+          height: 104,
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                top: 1,
+                right: 1,
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(8.0),
+                      topRight: Radius.circular(8.0)),
+                  child: Container(
+                    color: Theme.of(context).colorScheme.secondaryContainer,
+                    width: 36.0,
+                    height: 36.0,
                   ),
-                  Wrap(
-                    direction: Axis.vertical,
-                    spacing: 5.0,
-                    children: <Widget>[
-                      Text(
-                        style: Theme.of(context).textTheme.titleSmall?.apply(color: Theme.of(context).colorScheme.onSecondaryContainer),
-                        'Team Ninja',
-                      ), //team name
-                      Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.group_outlined,
-                            color: Theme.of(context).colorScheme.secondary,
-                            size: 18.0,
-                            weight: 0.5,
-                          ),
-                          SizedBox(
-                            width: 7.0,
-                          ),
-                          Text(
-                            style: Theme.of(context).textTheme.bodySmall?.apply(color: Theme.of(context).colorScheme.secondary),
-                            '5/10',
-                          ),
-                        ],
-                      ), //num of team members
-                    ],
-                  ),
-                ],
+                ), //purple bg
               ),
-            ),
-          ],
+              const Positioned(
+                top: 4,
+                right: 4,
+                child: CircleAvatar(
+                  radius: 15.0,
+                  foregroundImage: AssetImage('assets/images/logos/BS.png')), //game logo on top right hand corner
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12.0, 15.0, 12.0, 15.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 38.0,
+                      foregroundImage: (AssetImage('assets/images/logos/${imageString.isEmpty ? 'Unify.png' : imageString}')),
+                    ), //team avatar
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Wrap(
+                      direction: Axis.vertical,
+                      spacing: 5.0,
+                      children: <Widget>[
+                        Text(
+                          style: Theme.of(context).textTheme.titleSmall?.apply(color: Theme.of(context).colorScheme.onSecondaryContainer),
+                          teamName,
+                        ), //team name
+                        Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.group_outlined,
+                              color: Theme.of(context).colorScheme.secondary,
+                              size: 18.0,
+                              weight: 0.5,
+                            ),
+                            const SizedBox(
+                              width: 7.0,
+                            ),
+                            Text(
+                              style: Theme.of(context).textTheme.bodySmall?.apply(color: Theme.of(context).colorScheme.secondary),
+                              '$memberListSize/$maxTeamSize',
+                            ),
+                          ],
+                        ), //num of team members
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/manage/screens/manage_page.dart
+++ lib/widgets/manage/screens/manage_page.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:unify/widgets/common/card/team_manage_card.dart';
 import 'package:unify/widgets/common/nav/bottom_navigation_bar.dart';
 import 'package:unify/widgets/common/nav/top_app_bar.dart';
 import 'package:go_router/go_router.dart';
+import 'package:http/http.dart' as http;
+import 'package:unify/utils/constants/unify_backend.dart' as unify_client;
 
 class ManagePage extends StatefulWidget {
   const ManagePage({super.key});
@@ -12,39 +18,146 @@ class ManagePage extends StatefulWidget {
 }
 
 class _ManagePageState extends State<ManagePage> {
+
+  Future<Map<String, dynamic>> getUserDetails() async {
+    final result = await http.get(
+        Uri.parse(unify_client.unifyProfileServiceUrl),
+        headers: {
+          HttpHeaders.authorizationHeader: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJCZW5qYW1pbkdhbiIsImlhdCI6MTcxMDY5OTE0MCwiZXhwIjoxNzExMjU0MjAyfQ.NwTjNQdOIfxW_9JAKlVvQYazdnMKCwZnouAjqyHUnmA",
+          HttpHeaders.contentTypeHeader: "application/json",
+        },);
+
+    if (result.statusCode == 200) {
+      // Parse the response body
+      final Map<String, dynamic> body = json.decode(result.body);
+      
+      // Log the parsed body
+      // print('Parsed body: $body');
+      
+      return body;
+    } else {
+      // Handle HTTP error
+      throw Exception('Failed to load user details: ${result.statusCode}');
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> fetchUserTeams(List<String> teamIds) async {
+  List<Map<String, dynamic>> teams = [];
+  
+  for (String teamId in teamIds) {
+    final teamResponse = await http.get(
+      Uri.parse('${unify_client.unifyServerBaseUrl}/team/$teamId'), // Replace with your actual API endpoint
+      headers: {
+        HttpHeaders.authorizationHeader: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJCZW5qYW1pbkdhbiIsImlhdCI6MTcxMDY5OTE0MCwiZXhwIjoxNzExMjU0MjAyfQ.NwTjNQdOIfxW_9JAKlVvQYazdnMKCwZnouAjqyHUnmA",
+        HttpHeaders.contentTypeHeader: "application/json",
+      },
+    );
+    
+    if (teamResponse.statusCode == 200) {
+      final Map<String, dynamic> teamData = json.decode(teamResponse.body);
+      teams.add(teamData);
+    } else {
+      throw Exception('Failed to fetch team details: ${teamResponse.statusCode}');
+    }
+  }
+  
+  return teams;
+}
+
+  late Future<Map<String, dynamic>> userDetailsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    userDetailsFuture = getUserDetails();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const TopAppBar(title: 'Mng. Teams/Scrims'),
         body: Container(
           alignment: Alignment.center,
-          margin: EdgeInsets.only(top: 15.0),
+          margin: const EdgeInsets.only(top: 15.0),
           child: SingleChildScrollView(
             scrollDirection: Axis.vertical,
-            child: Wrap(
-              direction: Axis.horizontal,
-              runSpacing: 20,
-              children: <Widget>[
-                TeamManageCard(),
-                TeamManageCard(),
-                TeamManageCard(),
-                TeamManageCard(),
-                TeamManageCard(),
-                TeamManageCard(),
-                TeamManageCard(),
-              ],
+            child: FutureBuilder<Map<String, dynamic>?>(
+              future: userDetailsFuture,
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                    return const CircularProgressIndicator();
+                  case ConnectionState.done:
+                    if (snapshot.hasError) {
+                      return Text('${snapshot.error}');
+                    } else if (snapshot.hasData) {
+                      Map<String, dynamic>? data = snapshot.data;
+                      // Access the 'user' field
+                      final Map<String, dynamic> user = data?['user'];
+                      // Access the 'teams' field under 'user'
+                      final List<dynamic> userTeamsIds = user['teams'];
+                      final List<String> stringUserTeamsIds = userTeamsIds.map((id) => id.toString()).toList();
+                      // print("TEAM IDS: $stringUserTeamsIds");
+                      final Future<List<Map<String, dynamic>>> userTeams = fetchUserTeams(stringUserTeamsIds);
+                      
+                      return FutureBuilder<List<Map<String, dynamic>>>(
+                        future: userTeams,
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState == ConnectionState.waiting) {
+                            return CircularProgressIndicator();
+                          } else if (snapshot.hasError) {
+                            return Text('${snapshot.error}');
+                          } else if (snapshot.hasData) {
+                            List<Map<String, dynamic>> teams = snapshot.data!;
+                            return ListView.builder(
+                              shrinkWrap: true,
+                              physics: NeverScrollableScrollPhysics(),
+                              itemCount: teams.length,
+                              itemBuilder: (context, index) {
+                                Map<String, dynamic> team = teams[index]['bsTeam'];
+                                String teamId = team['_id'];
+                                String teamName = team['teamName'];
+                                String region = team['region'];
+                                String language = team['language'];
+                                String imageString = team['imageString'] ?? "";
+                                int maxTeamSize = team['maximumTeamSize'];
+                                int memberListSize = team['memberList'].length;
+                                return TeamManageCard(
+                                  teamId: teamId,
+                                  teamName: teamName,
+                                  gameName: "Brawl Stars",
+                                  region: region,
+                                  language: language,
+                                  imageString: imageString,
+                                  maxTeamSize: maxTeamSize,
+                                  memberListSize: memberListSize,
+                                );
+                              },
+                            );
+                          } else {
+                            return const Text('No teams to display :(');
+                          }
+                        },
+                      );
+                    } else {
+                      return const Text('No teams to display :(');
+                    }
+                  default:
+                    return const Text('Unexpected Error');
+                }
+              },
             ),
           ),
         ),
-        floatingActionButton: Container(
+        floatingActionButton: SizedBox(
           height: 100.0,
           width: 100.0,
           child: FittedBox(
             child: FloatingActionButton(
               backgroundColor: Theme.of(context).colorScheme.primaryContainer, 
               foregroundColor: Theme.of(context).colorScheme.onPrimaryContainer, 
-              onPressed: () => {context.go("/home")}, 
-              child: Icon(Icons.add)
+              onPressed: () => {context.go("/create_team/:id")}, 
+              child: const Icon(Icons.add)
             ),
           ),
         ),
