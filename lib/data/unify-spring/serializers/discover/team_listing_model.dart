e8714bd91aabe2fd9eb15c851e1b2559698a5d77
Shinkeishi <ashleyytanjx@gmail.com>
2024-03-18 19:50:35 +0800

    Create teamlisting model

--- lib/data/unify-spring/serializers/discover/team_listing_model.dart
+++ lib/data/unify-spring/serializers/discover/team_listing_model.dart
@@ -7,6 +7,8 @@ class TeamListing {
   final int wins3v3;
   final int wins2v2;
   final int soloWins;
+  final int maximumTeamSize;
+  final int currentTeamSize;
 
   TeamListing(
       {required this.id,
@@ -16,7 +18,9 @@ class TeamListing {
       required this.trophies,
       required this.wins3v3,
       required this.wins2v2,
-      required this.soloWins});
+      required this.soloWins,
+      required this.maximumTeamSize,
+      required this.currentTeamSize});
 
   factory TeamListing.fromMap(Map<String, dynamic> map) {
     return TeamListing(
@@ -27,6 +31,8 @@ class TeamListing {
         trophies: map["trophyRequirements"],
         wins3v3: map["min3v3Wins"],
         wins2v2: map["minDuoWins"],
-        soloWins: map["minSoloWins"]);
+        soloWins: map["minSoloWins"],
+        maximumTeamSize: map["maximumTeamSize"],
+        currentTeamSize: map["currentTeamSize"]);
   }
 }
