90ab269b3a02335a9937165e5d57877092888814
adversesg <faviancheong22@gmail.com>
2024-03-19 15:42:39 +0800

    Image replacement feature and finish all static components

--- lib/widgets/manage/screens/editteam_page.dart
+++ lib/widgets/manage/screens/editteam_page.dart
@@ -1,7 +1,9 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:unify/utils/constants/filter_dropdown_choices.dart';
 import 'package:unify/widgets/common/nav/bottom_navigation_bar.dart';
 import 'package:unify/widgets/common/nav/top_app_bar.dart';
+import 'package:image_picker/image_picker.dart';
 
 class EditTeamPage extends StatefulWidget {
   const EditTeamPage({super.key});
@@ -10,7 +12,80 @@ class EditTeamPage extends StatefulWidget {
   State<EditTeamPage> createState() => _EditTeamPageState();
 }
 
+class AvatarChanger extends StatefulWidget {
+  @override
+  _AvatarChangerState createState() => _AvatarChangerState();
+}
+
+class _AvatarChangerState extends State<AvatarChanger> {
+  File? _imageFile;
+  final picker = ImagePicker();
+
+  Future<void> _pickImage(ImageSource source) async {
+    final pickedFile = await picker.getImage(source: source);
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text("Change Image"),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.photo_library),
+                    title: Text("Select from Gallery"),
+                    onTap: () {
+                      Navigator.pop(context);
+                      _pickImage(ImageSource.gallery);
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.camera_alt),
+                    title: Text("Take a Photo"),
+                    onTap: () {
+                      Navigator.pop(context);
+                      _pickImage(ImageSource.camera);
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+      child: CircleAvatar(
+        radius: 70.0,
+        backgroundColor: Theme.of(context).colorScheme.onSecondary,
+        backgroundImage: _imageFile != null
+            ? FileImage(_imageFile!)
+            : AssetImage("assets/images/logos/Unify.png")
+                as ImageProvider<Object>?,
+        // child: _imageFile == null
+        //     ? Icon(Icons.add_a_photo, size: 40.0)
+        //     : null, // Placeholder icon if no image is selected
+      
+      ),
+    );
+  }
+}
+
 class _EditTeamPageState extends State<EditTeamPage> {
+  TextEditingController _teamNameController =
+      TextEditingController(text: 'Team Ninja');
+  TextEditingController _maxTeamSizeController =
+      TextEditingController(text: '10');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,16 +100,7 @@ class _EditTeamPageState extends State<EditTeamPage> {
           Stack(
             alignment: AlignmentDirectional.topCenter,
             children: <Widget>[
-              CircleAvatar(
-                radius: 70.0,
-                backgroundColor: Theme.of(context).colorScheme.onSecondary,
-                backgroundImage: AssetImage("assets/images/logos/Unify.png"),
-                // child: Icon(
-                //   const IconData(0xe130, fontFamily: 'MaterialIcons'),
-                //   color: Theme.of(context).colorScheme.secondary,
-                //   size: 40.0,
-                // ),
-              ),
+              AvatarChanger(),
               Positioned(
                 bottom: 10,
                 right: 130,
@@ -68,7 +134,7 @@ class _EditTeamPageState extends State<EditTeamPage> {
               width: 370,
               child: TextField(
                 //State management for region
-                // controller: _bioTextController,
+                controller: _teamNameController,
                 // onChanged: (newValue) {
                 //   _controller.updateUserPersonalBio(newValue);
                 // },
@@ -103,7 +169,7 @@ class _EditTeamPageState extends State<EditTeamPage> {
               width: 370,
               child: TextField(
                 //State management for region
-                // controller: _bioTextController,
+                controller: _maxTeamSizeController,
                 // onChanged: (newValue) {
                 //   _controller.updateUserPersonalBio(newValue);
                 // },
@@ -121,7 +187,7 @@ class _EditTeamPageState extends State<EditTeamPage> {
             height: 10.0,
           ),
           Padding(
-            padding: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(5.0),
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
@@ -129,9 +195,6 @@ class _EditTeamPageState extends State<EditTeamPage> {
                       style: Theme.of(context).textTheme.titleMedium?.apply(
                           color: Theme.of(context).colorScheme.secondary),
                       "Members"),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
                 ]),
           ),
           //List of members
@@ -139,20 +202,115 @@ class _EditTeamPageState extends State<EditTeamPage> {
             color: Colors.transparent, // Set the color to transparent
             elevation: 0, // Optionally, set the elevation to 0 to remove shadow
             child: ListTile(
+              contentPadding: EdgeInsets.fromLTRB(3, 0, 3, 0),
               leading: ClipRRect(
-                borderRadius: BorderRadius.circular(5), // Adjust the border radius as needed
+                borderRadius: BorderRadius.circular(
+                    5), // Adjust the border radius as needed
                 child: Image.asset(
                   'assets/images/avatars/BenjaminGan.png',
                   width: 40, // Set the desired width
                   height: 40, // Set the desired height
                 ),
               ),
-              title: Text('Benjamin Gan'),
+              title: Text(
+                  style: Theme.of(context).textTheme.titleMedium?.apply(
+                      color: Theme.of(context).colorScheme.onTertiaryContainer),
+                  'Benjamin Gan'),
               trailing: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  Text('114,918'),
-                  SizedBox(width:10), 
+                  Text(
+                    '114,918',
+                    style: Theme.of(context).textTheme.titleMedium?.apply(
+                          color:
+                              Theme.of(context).colorScheme.onTertiaryContainer,
+                          fontSizeDelta: -3.5, // Adjust the font size as needed
+                        ),
+                  ),
+                  SizedBox(width: 10),
+                  Image.asset(
+                    'assets/images/brawlstars/misc/Trophy.png', // Replace with your image path
+                    width: 24, // Set the desired width
+                    height: 24, // Set the desired height
+                  ),
+                  // Adjust the spacing between the image and text
+                ],
+              ),
+            ),
+          ),
+
+          //Hard code the other cards
+          Card(
+            color: Colors.transparent, // Set the color to transparent
+            elevation: 0, // Optionally, set the elevation to 0 to remove shadow
+            child: ListTile(
+              contentPadding: EdgeInsets.fromLTRB(3, 0, 3, 0),
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(
+                    5), // Adjust the border radius as needed
+                child: Image.asset(
+                  'assets/images/avatars/BenjaminGan.png',
+                  width: 40, // Set the desired width
+                  height: 40, // Set the desired height
+                ),
+              ),
+              title: Text(
+                  style: Theme.of(context).textTheme.titleMedium?.apply(
+                      color: Theme.of(context).colorScheme.onTertiaryContainer),
+                  'Dexter'),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    '114,918',
+                    style: Theme.of(context).textTheme.titleMedium?.apply(
+                          color:
+                              Theme.of(context).colorScheme.onTertiaryContainer,
+                          fontSizeDelta: -3.5, // Adjust the font size as needed
+                        ),
+                  ),
+                  SizedBox(width: 10),
+                  Image.asset(
+                    'assets/images/brawlstars/misc/Trophy.png', // Replace with your image path
+                    width: 24, // Set the desired width
+                    height: 24, // Set the desired height
+                  ),
+                  // Adjust the spacing between the image and text
+                ],
+              ),
+            ),
+          ),
+
+          Card(
+            color: Colors.transparent, // Set the color to transparent
+            elevation: 0, // Optionally, set the elevation to 0 to remove shadow
+            child: ListTile(
+              contentPadding: EdgeInsets.fromLTRB(3, 0, 3, 0),
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(
+                    5), // Adjust the border radius as needed
+                child: Image.asset(
+                  'assets/images/avatars/BenjaminGan.png',
+                  width: 40, // Set the desired width
+                  height: 40, // Set the desired height
+                ),
+              ),
+              title: Text(
+                  style: Theme.of(context).textTheme.titleMedium?.apply(
+                      color: Theme.of(context).colorScheme.onTertiaryContainer),
+                  'Mary Jane'),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    '114,918',
+                    style: Theme.of(context).textTheme.titleMedium?.apply(
+                          color:
+                              Theme.of(context).colorScheme.onTertiaryContainer,
+                          fontSizeDelta: -3.5, // Adjust the font size as needed
+                        ),
+                  ),
+                  SizedBox(width: 10),
                   Image.asset(
                     'assets/images/brawlstars/misc/Trophy.png', // Replace with your image path
                     width: 24, // Set the desired width
