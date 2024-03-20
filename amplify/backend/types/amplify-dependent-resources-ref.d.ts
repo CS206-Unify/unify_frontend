f319affff91a6813f04ecf32f51f908ef6fffc4b
Shinkeishi <ashleyytanjx@gmail.com>
2024-03-20 22:49:02 +0800

    Installed amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "unifyfrontendd567d59c": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "images": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:unify/model/discover/filters_model.dart';
 import 'package:unify/utils/theme/theme.dart';
 import 'package:unify/router.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_storage_s3/amplify_storage_s3.dart';
+import 'amplifyconfiguration.dart';
+
+Future<void> _configureAmplify() async {
+  try {
+    await Amplify.addPlugins([AmplifyAuthCognito(), AmplifyStorageS3()]);
+
+// call Amplify.configure to use the initialized categories in your app
+
+    await Amplify.configure(amplifyconfig);
+  } on Exception catch (e) {
+    safePrint('An error occurred configuring Amplify: $e');
+  }
+}
 
 void main() async {
+  await dotenv.load(fileName: "../.env"); // Load the .env file
+  await _configureAmplify();
   runApp(const Unify());
 }
 
